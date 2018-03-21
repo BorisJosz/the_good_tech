e7fbf8e09ad1f7d4351c31db83857f1a8ad40a27
Meg Vandezande <meg.vandezande@gmail.com>
2018-03-21 14:29:05 +0100

    add reviews controller and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @bookings = @user.bookings
+    @booking = Booking.find(params[:id])
     @items = @user.items
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,18 +2,17 @@ class Item < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
   belongs_to :owner, class_name: 'User'
-  has_many :reviews, through: :booking
   has_many :bookings
 
-  validates :category, presence: true
+  validates :category,      presence: true
   validates :price_per_day, presence: true
-  validates :description, presence: true
-  validates :photo, presence: true
+  validates :description,   presence: true
+  validates :photo,         presence: true
 
   CATEGORIES = ["drill", "camera", "drone", "TV", "cat"]
 
   def self.categories
-    return CATEGORIES
+    CATEGORIES
   end
 
 end
