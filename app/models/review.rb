1a35a1fcb08e64037a5e03698b7e9799d189990b
Meg Vandezande <meg.vandezande@gmail.com>
2018-03-23 09:43:21 +0100

    meg review fix

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,11 +4,11 @@ class Review < ApplicationRecord
 
   validates :description, presence: true
   validates :stars, presence: true, inclusion: { in: (1..5) }
-  validate :validate_one_review_only
+  # validate :validate_one_review_only
 
-  def validate_one_review_only
-    errors.add(:base, "errrrr") if self.class.joins(:booking).where(bookings: {user: user}).exists?
-  end
+  # def validate_one_review_only
+    # errors.add(:base, "errrrr") if self.class.joins(:booking).where(bookings: {user: user}).exists?
+  # end
 
 
 end
